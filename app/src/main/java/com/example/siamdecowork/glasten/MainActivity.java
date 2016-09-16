28bdcbafe406c5a716ff9e4b3f0e37323c17f202
ball <ballzacup1@gmail.com>
2016-09-16 13:34:05 +0700

    Check Space Fisish

--- app/src/main/java/com/example/siamdecowork/glasten/MainActivity.java
+++ app/src/main/java/com/example/siamdecowork/glasten/MainActivity.java
@@ -2,12 +2,64 @@ package com.example.siamdecowork.glasten;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, widthEditText,
+    heightEditText, detailEditText;
+    private ImageView imageView;
+    private RadioGroup radioGroup;
+    private RadioButton doorRadioButton, windowRadioButton;
+    private String nameString, widthString, heighString,
+            deetailString, typeString, imageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        widthEditText = (EditText) findViewById(R.id.editText2);
+        heightEditText = (EditText) findViewById(R.id.editText3);
+        detailEditText = (EditText) findViewById(R.id.editText4);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        radioGroup = (RadioGroup) findViewById(R.id.ragType);
+        doorRadioButton = (RadioButton) findViewById(R.id.radioButton);
+        windowRadioButton = (RadioButton) findViewById(R.id.radioButton2);
+    }// Main Method
+
+
+    public void clickSaveDataMain(View view) {
+
+        //Get Value From Edit Text
+        nameString = nameEditText.getText().toString().trim();
+        widthString = widthEditText.getText().toString().trim();
+        heighString = heightEditText.getText().toString().trim();
+        deetailString = detailEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || widthString.equals("") ||
+                heighString.equals("") || deetailString.equals("")) {
+            // Have Space
+
+            MyAlert myAlert = new MyAlert(this, R.drawable.doremon48,
+                    "มีช่องว่าง", "กรุณา กรอก ช่องค่ะ");
+            myAlert.myDialog();
+
+        }
+
+    }// Click Save
+
+    public void ClickListDataMain(View view) {
+
     }
-}
+
+
+} // Main Class
