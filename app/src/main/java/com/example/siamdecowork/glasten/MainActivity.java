6c8999c9163618d16e946a914824ee26e2eaa4e9
ball <ballzacup1@gmail.com>
2016-09-16 14:47:16 +0700

    Find Path Image

--- app/src/main/java/com/example/siamdecowork/glasten/MainActivity.java
+++ app/src/main/java/com/example/siamdecowork/glasten/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.siamdecowork.glasten;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -17,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton doorRadioButton, windowRadioButton;
     private String nameString, widthString, heighString,
-            deetailString, typeString, imageString;
+            deetailString, typeString, imageString,
+    imagePathString, imageFileString;
+    private boolean imageABoolean = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +40,64 @@ public class MainActivity extends AppCompatActivity {
         radioGroup = (RadioGroup) findViewById(R.id.ragType);
         doorRadioButton = (RadioButton) findViewById(R.id.radioButton);
         windowRadioButton = (RadioButton) findViewById(R.id.radioButton2);
+
+        //Image Conttroller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,
+                        "โปรดเลือกรูปภาพ"), 1);
+
+            }// OnClick
+        });
+
     }// Main Method
 
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == 1) && (resultCode == RESULT_OK)) {
+
+            imageABoolean = false;
+
+            // FindI path of Image
+
+            Uri uri = data.getData();
+            imagePathString = myFindPathImage(uri);
+
+            Log.d("16SepV1", "imagePathString ==>" + imagePathString);
+
+        }// if
+
+    }// onActivity
+
+    private String myFindPathImage(Uri uri) {
+
+        String strResult = null;
+
+        String[] strings = {MediaStore.Video.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings,
+                null, null, null);
+
+        if (cursor != null) {
+
+            cursor.moveToFirst();
+            int intIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            strResult = cursor.getString(intIndex);
+
+        } else {
+            strResult = uri.getPath();
+        }
+
+
+        return strResult;
+    }
 
     public void clickSaveDataMain(View view) {
 
@@ -53,7 +116,21 @@ public class MainActivity extends AppCompatActivity {
                     "มีช่องว่าง", "กรุณา กรอก ช่องค่ะ");
             myAlert.myDialog();
 
+        } else if (!(doorRadioButton.isChecked() || windowRadioButton.isChecked())) {
+            // Un Check RadioButton
+            MyAlert myAlert = new MyAlert(this, R.drawable.nobita48,
+                    "ยังไม่เลือกชนิด", "กรุณาเลือก ชนิด ด้วยค่ะ");
+            myAlert.myDialog();
+        } else if (imageABoolean) {
+            //Non Choose Image
+            MyAlert myAlert = new MyAlert(this, R.drawable.bird48,
+                    "ยังไม่ได้เลือกรูป", "กรุณาคลิกที่รูป และเลือกรูป");
+            myAlert.myDialog();
+        } else {
+            // Complete Choose Image
         }
+
+        Log.d("16SepV1", "imageBoolean ==> " + imageABoolean);
 
     }// Click Save
 
